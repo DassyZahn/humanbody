527b56c5fa4a34fb73a6163b2983393f545d3ad1
Dassy Zahn <dassysilkin@gmail.com>
2023-06-14 18:17:19 +0300

    made changes

--- 1 table/1 table.sql
+++ 1 table/1 table.sql
@@ -1,3 +1,8 @@
 use humanbodydb
 go
-create table dbo.body(id int not null identity primary key)
+drop table if EXISTS body
+create table dbo.body(
+    id int not null identity primary key,
+    bodypart varchar(100) not null,
+    num int null
+    )
